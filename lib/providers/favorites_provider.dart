bb4ac63c2698766f0230af85a942cb219d6db1ed
Jignesh5049 <prajapatijignesh225@gmail.com>
2026-02-05 14:42:50 +0530

    ver 1.0

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/meal.dart';
+
+class FavoriteMealsNotifier extends Notifier<List<Meal>> {
+  @override
+  List<Meal> build() {
+    return [];
+  }
+
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    NotifierProvider<FavoriteMealsNotifier, List<Meal>>(() {
+      return FavoriteMealsNotifier();
+    });
